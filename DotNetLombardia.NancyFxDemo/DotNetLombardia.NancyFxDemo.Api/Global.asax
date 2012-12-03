d212d5e9d08e516b261c0661771fed2be64c0420
Michele Capra <michele@orangecode.it>
2012-12-03 11:59:00 -0800

    Solution setup

---
+++ DotNetLombardia.NancyFxDemo/DotNetLombardia.NancyFxDemo.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetLombardia.NancyFxDemo.Api.MvcApplication" Language="C#" %>
---
+++ DotNetLombardia.NancyFxDemo/DotNetLombardia.NancyFxDemo.Api/Modules/ApiModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Nancy;
+using Nancy.Security;
+
+namespace DotNetLombardia.NancyFxDemo.Api.Modules
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule()
+        {
+            //this.RequiresAuthentication();
+
+            Get[@"/api"] = r =>
+            {
+                IList<object> services = new List<object>();
+                
+                return Response.AsJson(services);
+            };
+        }
+
+    }
+}
